1cea3cb98f72a281cd866ed9eb2acc4e168bba84
officialsanchi <nwangwudinma22@gmail.com>
2025-08-11 11:49:43 +0100

    added some dependency

--- src/main/java/com/example/ClaryFi/domain/model/Company.java
+++ src/main/java/com/example/ClaryFi/domain/model/Company.java
@@ -1,10 +1,23 @@
 package com.example.ClaryFi.domain.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
 import java.util.UUID;
 
-
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="company")
 public class Company {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
     private String email;
